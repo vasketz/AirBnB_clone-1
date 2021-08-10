1faad782bb73a25c4ff5146a1302b81bea7829d7
root <root@704001abdf99>
2021-08-10 16:33:37 +0000

    Update task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if cls == None:
             return FileStorage.__objects
         new_list = {}
         for key, value in FileStorage.__objects.items():
