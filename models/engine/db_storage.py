b12a700390baaa3ccdad5c96c9a4da63ab8108d6
MattHenao <2340@holbertonschool.com>
2021-08-11 20:09:43 -0500

    Update task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+""" Module DBStorage """
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Places
+from models.review import Review
+
+
+class DBStorage:
+    """ Class DBStorage """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        USER = getenv('HBNB_MYSQL_USER')
+        PASSWORD = getenv('HBNB_MYSQL_PWD')
+        HOST = getenv('HBNB_MYSQL_HOST')
+        DB = getenv('HBNB_MYSQL_DB')
+        ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+                                      USER, PASSWORD, HOST, DB),
+                                      pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ Specify the classes to query """
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            valueQuery = self.__session.query(cls)
+        else:
+            valueQuery = self.__session.query(State).all()
+            valueQuery.extend(self.__session.query().all(City))
+            valueQuery.extend(self.__session.query().all(User))
+            valueQuery.extend(self.__session.query().all(Place))
+            valueQuery.extend(self.__session.query().all(Review))
+            valueQuery.extend(self.__session.query().all(Amenity))
+        myDict = {}
+        for obj in valueQuery:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            myDict[key] = obj
+        return(myDict)
+
+    def new(self, obj):
+        """ Add a object to database """
+        self.__session.add(self)
+
+    def save(self):
+        """ Save a oject to database """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ Delete a object to database """
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ Create all tables to database """
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = Session()
+
+    def close(self):
+        """ Close the session """
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+from models import storage
+
+
+class State(BaseModel, Base):
+    """ Define data types of the table """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable = False)
+    cities = relationship('City', backref = 'state', cascade = 'all, delete')
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ Return a new list with the object of cities """
+            cityList = []
+            obj_city = storage.all(City)
+            for items in obj_city.value():
+                if items.state_id == self.id:
+                    cityList.append(items)
+            return cityList
