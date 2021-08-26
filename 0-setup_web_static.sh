7fd46bcfe82cfe7e590b8a643d541a15500aa63d
vasketz <anfevach@gmail.com>
2021-08-26 20:17:58 +0000

    update task 0 of deploy

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers 
+# for the deployment of web_static
+sudo apt-get update
+sudo apt-get install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "nginx testing" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "48 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
