7ff6dba67b0d126b39b9a95da26697f8e8dbe92c
vasketz <anfevach@gmail.com>
2021-08-26 20:42:55 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,5 @@ sudo mkdir -p /data/web_static/releases/test
 echo "nginx testing" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "48 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "48 i \\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
