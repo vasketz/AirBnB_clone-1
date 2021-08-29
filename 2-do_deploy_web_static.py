4f823a093b6f65a09a7e43be456f5d51e23c80a0
vasketz <anfevach@gmail.com>
2021-08-29 22:35:58 +0000

    update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,8 +9,10 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
-env.user = "ubuntu"
-env.hosts = ["34.74.231.67", "54.172.241.181"]
+
+env.user = 'ubuntu'
+env.hosts = ['34.74.231.67', '54.172.241.181']
+
 
 def do_pack():
     """
@@ -32,22 +34,19 @@ def do_deploy(archive_path):
     """
     if path.exists(archive_path) is False:
         return False
-    
-    try:
-        put(archive_path, "/tmp/")
-        file = archive_path.replace(".tgz", "")
-        file = file.replace("versions/", "")
-        run("mkdir -p /data/web_static/releases/{}/".format(file))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
-             file, file))
-        run("rm -r /tmp/{}.tgz".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* ".format(
-             file) + "/data/web_static/releases/{}/".format(file))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current/".
-        format(file))
-        print("New version deployed!")
-        return True
-    except:
-        return False
+    file = archive_path.split("/")[1]
+    path_folder = file[:-4]
+
+    put(archive_path, "/tmp/")
+    run("mkdir -p /data/web_static/releases/{}/".format(path_folder))
+    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
+        file, path_folder))
+    run("rm -r /tmp/{}".format(file))
+    run("mv /data/web_static/releases/{}/web_static/* ".format(
+        path_folder) + "/data/web_static/releases/{}".format(path_folder))
+    run("rm -rf /data/web_static/releases/{}/web_static".format(path_folder))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{} /data/web_static/current/".format(
+        path_folder))
+    print("New version deployed!")
+    return True
