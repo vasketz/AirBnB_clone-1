cd4bb998d482abc5a9480aa4b67ac90f8f872cef
vasketz <anfevach@gmail.com>
2021-08-29 21:55:07 +0000

    Update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,8 @@ from fabric.api import *
 from datetime import datetime
 from os import path
 
+env.user = "ubuntu"
+env.hosts = ["34.74.231.67", "54.172.241.181"]
 
 def do_pack():
     """
@@ -20,7 +22,7 @@ def do_pack():
         file = "versions/web_static_{}.tgz".format(date_time)
         local("tar -czvf {} web_static".format(file))
         return file
-    else:
+    except:
         return None
 
 
@@ -28,8 +30,24 @@ def do_deploy(archive_path):
     """
     descompress tar
     """
-    if path.exist(archive_path) is False:
+    if path.exists(archive_path) is False:
         return False
-    file = archive_path.split("/")
-
-    put(archive_path, "/tmp/")
+    
+    try:
+        put(archive_path, "/tmp/")
+        file = archive_path.replace(".tgz", "")
+        file = file.replace("versions/", "")
+        run("mkdir -p /data/web_static/releases/{}/".format(file))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(
+             file, file))
+        run("rm -r /tmp/{}.tgz".format(file))
+        run("mv /data/web_static/releases/{}/web_static/* ".format(
+             file) + "/data/web_static/releases/{}/".format(file))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current/".
+        format(file))
+        print("New version deployed!")
+        return True
+    except:
+        return False
