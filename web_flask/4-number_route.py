ff9949846b6e65ab2bd36e5cb04c2e40199d0d86
vasketz <anfevach@gmail.com>
2021-09-12 18:07:51 +0000

    Update task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This script starts a flask web application
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Funtion to print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print from /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """Display c from variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text):
+    """display is cool by default"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """Display a int only if it is an int"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
