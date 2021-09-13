335b41ea7b85ebe2791206c37a5938d36ffe44ce
vasketz <anfevach@gmail.com>
2021-09-13 00:32:53 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -13,13 +13,14 @@ class State(BaseModel, Base):
     """ Define data types of the table """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+    cities = relationship('City', backref='state')
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """ Return a new list with the object of cities """
             from models import storage
+            from models.city import City
             cityList = []
             obj_city = storage.all(City)
             for items in obj_city.value():
