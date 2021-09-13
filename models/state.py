9e665c63832e46ed4d3a19f6b1e0f7d75a27115b
vasketz <anfevach@gmail.com>
2021-09-13 00:34:11 +0000

    uPDATE STATES

--- models/state.py
+++ models/state.py
@@ -5,7 +5,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
 from os import getenv
 
 
